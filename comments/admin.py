7a8f0fe01bdfffd45b56978e93e9a4fed72a2b3d
Moldaspan <ersultanmoldaspanov@mail.ru>
2025-02-05 02:23:00 +0500

    comments upd

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'publication', 'content', 'created_at', 'updated_at')
+    search_fields = ('author__email', 'publication__title', 'content')
+    list_filter = ('created_at', 'updated_at')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Comment, CommentAdmin)
