b01b7dbb3ccc5ce94893fcba764c8e45db6fb0c7
Moldaspan <ersultanmoldaspanov@mail.ru>
2025-02-05 02:24:39 +0500

    info upd

--- info/admin.py
+++ info/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Feedback, FeedbackImage
 
-# Register your models here.
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'phone_number', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
+    list_filter = ('created_at',)
+
+@admin.register(FeedbackImage)
+class FeedbackImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'feedback', 'image')
