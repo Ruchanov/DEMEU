e5642123e6b683572dfb59aa3a6326cf25a92a1c
Moldaspan <ersultanmoldaspanov@mail.ru>
2025-02-05 02:23:48 +0500

    favorites upd

--- favorites/admin.py
+++ favorites/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import FavoritePublication, FavoriteUser
 
-# Register your models here.
+
+class FavoritePublicationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'publication', 'created_at')
+    search_fields = ('user__email', 'publication__title')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+
+
+class FavoriteUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'favorite_user', 'created_at')
+    search_fields = ('user__email', 'favorite_user__email')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+
+
+admin.site.register(FavoritePublication, FavoritePublicationAdmin)
+admin.site.register(FavoriteUser, FavoriteUserAdmin)
