e9d1b1130dc72292b5c65e984f59efb7c5afcf8b
Moldaspan <ersultanmoldaspanov@mail.ru>
2025-02-05 02:25:53 +0500

    publications upd

--- publications/admin.py
+++ publications/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Publication, PublicationImage, PublicationVideo, Donation, View
 
-# Register your models here.
+
+class PublicationImageInline(admin.TabularInline):
+    model = PublicationImage
+    extra = 1  # Позволяет загружать дополнительные изображения
+
+
+class PublicationVideoInline(admin.TabularInline):
+    model = PublicationVideo
+    extra = 1  # Позволяет загружать дополнительные видео
+
+
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'amount', 'created_at')
+    search_fields = ('title', 'author__email', 'category')
+    list_filter = ('category', 'created_at')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+    inlines = [PublicationImageInline, PublicationVideoInline]
+
+    fieldsets = (
+        ('Основная информация', {'fields': ('author', 'title', 'category', 'description')}),
+        ('Финансовая информация', {'fields': ('bank_details', 'amount')}),
+        ('Контактные данные', {'fields': ('contact_name', 'contact_email', 'contact_phone')}),
+        ('Даты', {'fields': ('created_at', 'updated_at')}),
+    )
+
+
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('donor_name', 'donor_amount', 'publication')
+    search_fields = ('donor_name', 'donor_email', 'publication__title')
+    list_filter = ('publication',)
+
+
+class ViewAdmin(admin.ModelAdmin):
+    list_display = ('viewer', 'publication', 'viewed_at')
+    search_fields = ('viewer__email', 'publication__title')
+    list_filter = ('viewed_at',)
+
+
+admin.site.register(Publication, PublicationAdmin)
+admin.site.register(Donation, DonationAdmin)
+admin.site.register(View, ViewAdmin)
