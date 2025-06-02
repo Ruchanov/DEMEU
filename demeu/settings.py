1c3b4f9f51b0cb643bef4e6179a3b08c32e02794
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2025-06-02 15:02:24 +0500

    asd

--- demeu/settings.py
+++ demeu/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
+
 
 
 # Application definition
